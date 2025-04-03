f06afb845b12c7fcb5b631e8cd135279b9e32336
Cbermont <89049830+bermont13@users.noreply.github.com>
2025-04-03 03:36:17 +0000

    tarea entregada modelo datos starwars

--- src/models.py
+++ src/models.py
@@ -14,8 +14,7 @@ class User(db.Model):
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
     favorites = relationship("Favorites", back_populates="user")
-    posts = relationship("Post", back_populates="author")
-    comments = relationship("Comment", back_populates="author")
+    
 
     def serialize(self):
         return {
@@ -47,19 +46,5 @@ class Favorites(db.Model):
 
     user = relationship("User", back_populates="favorites")
 
-class Post(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    content: Mapped[str] = mapped_column(String(300), nullable=False)
-    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
 
-    author = relationship("User", back_populates="posts")
-    comments = relationship("Comment", back_populates="post")
-
-class Comment(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    comment_text: Mapped[str] = mapped_column(String(255), nullable=False)
-    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
-    post_id: Mapped[int] = mapped_column(ForeignKey('post.id'), nullable=False)
-
-    author = relationship("User", back_populates="comments")
-    post = relationship("Post", back_populates="comments")
+    
